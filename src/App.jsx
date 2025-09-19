5431822b052b8fd93ddbefc28c19add3ef457823
Sanjyrana <Sanjayrana5113@gmail.com>
2025-09-19 21:55:56 +0530

    created marque animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import CompanyLogo from './components/CompanyLogo'
+import './App.css'
 
 const App = () => {
   return (
     <main className='relative min-h-screen overflow-x-hidden'>
-    <div className='absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr  from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10'>
-
-    </div>
-      <div className='overflow-hidden'>  
-      <Navbar />
+      <div className='absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr  from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10'>
+      </div>
+      <div className='overflow-hidden'>
+        <Navbar />
         <Hero />
-        </div>
+        <CompanyLogo/>
+      </div>
     </main>
   )
 }
---
+++ src/components/CompanyLogo.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import slack from "../assets/slack.png"
+import amazon from "../assets/amazon.png"
+import wooCommerce from "../assets/woocommerce.png"
+import meundies from "../assets/meundies.png"
+import sitepoint from "../assets/sitepoint.png"
+
+function CompanyLogo() {
+  const logo = [slack,amazon,wooCommerce,meundies,sitepoint]
+  return (
+    <div className='w-full container mx-auto py-20 flex sm:flex-row flex-col gap-8 sm:items-center items-start overflow-hidden'>
+        <div className='w-[300px] text-gray-600 border-l-4 border-blue-500 bg-white px-4 py-2  z-10 ssm:text-base text-xl font-semibold text-left'>Proud Partner at <br /> Hubspot & Segment</div>
+
+        <div className='flex animate-marquee whitespace-nowrap'>
+         
+           {logo.map((logo,index)=>{
+            return <img key={index} src={logo} alt="comapnay logo"
+            className='mx-12 h-8 w-36 object-contain grayscale opacity-70 hover:grayscale-0 hover:opacity-100 transition-all' />
+          })}
+         
+        </div>
+      </div>
+
+  )
+}
+
+export default CompanyLogo
